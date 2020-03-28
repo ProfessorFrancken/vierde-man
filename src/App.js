0019c58106f1eac672b4d182f3a238d64fbac12c
Mark Redeman <markredeman@gmail.com>
2020-03-28 13:45:52 +0100

    Extract Header coponent

--- src/App.js
+++ src/App.js
@@ -1,70 +1,14 @@
 import React from 'react';
 import { Client } from 'boardgame.io/react';
 import { KlaverJassen } from 'GameLogic/Game';
-import { useForm } from 'react-hook-form';
-import PlaceBid, { CurrentBids, Bid } from 'App/PlaceBid';
-import { CurrentPlayedCards } from 'App/PlayTrick';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFileAlt } from '@fortawesome/free-solid-svg-icons';
-// import { PlaceBid, Pass } from 'Phases/PlaceBids';
+import PlaceBid from 'App/PlaceBid';
 import { playerIsAllowedToPlayerCard } from 'GameLogic/Phases/PlayTricks';
-import logo from './logo.svg';
 import './App.css';
 import styled from 'styled-components';
-import {
-  SuitStringToComponent,
-  Spades,
-  Hearts,
-  Diamonds,
-  Clubs,
-  Sans
-} from 'Components/Suits';
-import { SUITES, SANS } from 'GameLogic/Card';
-const { SPADES, HEARTS, CLUBS, DIAMONDS } = SUITES;
+import { SuitStringToComponent } from 'Components/Suits';
+import Header from 'App/Header';
 
 const isSans = event => event.value === 'sans';
-
-const Header = ({ game, phase, currentBids, currentPlayer }) => {
-  return (
-    <div className="w-100 bg-primary">
-      <div className="d-flex justify-content-between align-items-center text-white p-3">
-        <div className="d-flex justify-content-between">
-          <ul className="list-unstyled mb-0 d-flex -justify-content-between ">
-            <li className="bg-dark text-white px-3 py-1 rounded">
-              <FontAwesomeIcon icon={faFileAlt} />
-            </li>
-            <li className="bg-dark text-white px-3 py-1 rounded mx-2">
-              <strong>Wij</strong>: {game.wij}
-            </li>
-            <li className="bg-dark text-white px-3 py-1 rounded mx-2">
-              <strong>Zij</strong>: {game.zij}
-            </li>
-          </ul>
-          {/* <div> */}
-          {/*   Wij: {game.wij} | Zij: {game.zij} */}
-          {/*   {/\* Wij: {G.rounds.filter(({ winner }) => [0, 2].includes(winner)).map(({ points })} | Zij: {G.zij} *\/} */}
-          {/* </div> */}
-        </div>
-        <div>
-          {game.bid.trump !== undefined ? (
-            <Bid bid={{ bid: game.bid.bid, suit: game.bid.trump }} />
-          ) : (
-            <span>Select a bid</span>
-          )}
-        </div>
-      </div>
-      {phase === 'PlaceBids' && (
-        <CurrentBids bids={currentBids} currentPlayer={currentPlayer} />
-      )}
-      {phase === 'PlayTricks' && (
-        <CurrentPlayedCards
-          currentTrick={game.currentTrick}
-          currentPlayer={currentPlayer}
-        />
-      )}
-    </div>
-  );
-};
 
 const bids = event =>
   isSans(event)
