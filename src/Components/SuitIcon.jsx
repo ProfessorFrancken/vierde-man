aa1330e7c73ae23914121ee3fb4e52eda197e9de
Mark Redeman <markredeman@gmail.com>
2020-03-29 15:17:11 +0200

    Use unicode font for suits

--- src/Components/SuitIcon.jsx
+++ src/Components/SuitIcon.jsx
@@ -1,8 +1,4 @@
 import React from 'react';
-import spades from 'assets/Spade.png';
-import hearts from 'assets/Heart.png';
-import diamonds from 'assets/Diamond.png';
-import clubs from 'assets/Club.png';
 import sans from 'assets/Sans2.png';
 
 const SuitIcon = props => {
@@ -13,10 +9,8 @@ const SuitIcon = props => {
   return <img src={props.src} style={imgStyle} alt={props.alt} />;
 };
 
-export const Spades = () => <SuitIcon src={spades} alt="Suit of spades" />;
-export const Hearts = () => <SuitIcon src={hearts} alt="Suit of hearts" />;
-export const Diamonds = () => (
-  <SuitIcon src={diamonds} alt="Suit of diamonds" />
-);
-export const Clubs = () => <SuitIcon src={clubs} alt="Suit of clubs" />;
+export const Spades = () => <span className="text-dark">♠</span>;
+export const Hearts = () => <span className="text-danger">♥</span>;
+export const Diamonds = () => <span className="text-danger">♦</span>;
+export const Clubs = () => <span className="text-dark">♣</span>;
 export const Sans = () => <SuitIcon src={sans} width="60px" alt="Sans" />;
