cdfceb4d4dcc3443a64eba0cefe5f2d69309cb10
Mark Redeman <markredeman@gmail.com>
2020-03-31 19:11:00 +0200

    Improve design of cards

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,21 +1,51 @@
 import React, { useState } from 'react';
 import { SuitStringToComponent } from 'Components/Suits';
 import styled from 'styled-components';
+import { SUITES } from 'GameLogic/Card';
+const { SPADES, HEARTS, CLUBS, DIAMONDS } = SUITES;
 
 // TODO: if player name is Sven or sbte, then shake cards
 
-const cardRatio = 1.5;
+const suitIcon = {
+  [SPADES]: '♠',
+  [HEARTS]: '♥',
+  [CLUBS]: '♣',
+  [DIAMONDS]: '♦'
+};
+const cardRatio = 2.5;
 const CardLi = styled.li`
   transition: all 0.3s ease-out;
   transform: rotate(${props => props.rotate}deg);
 
   width: ${() => 58 * cardRatio}px;
   height: ${() => 88 * cardRatio}px;
+  color: ${props =>
+    [HEARTS, DIAMONDS].includes(props.suit) ? '#e44145' : '#252525'} !important;
 
   :hover {
     transform: scale(1) rotate(${props => props.rotate}deg);
     cursor: pointer;
     background: white !important;
+  }
+
+  &:before,
+  &:after {
+    content: '${props => props.face}  ${props => suitIcon[props.suit]}';
+    position: absolute;
+    width: 12px;
+    text-align: center;
+    letter-spacing: -2px;
+  }
+
+  &:before {
+    top: 4px;
+    left: 4px;
+  }
+
+  &:after {
+    bottom: 4px;
+    right: 4px;
+    transform: rotate(180deg);
   }
 `;
 
@@ -28,7 +58,7 @@ const Card = props => {
   const [rotate] = useState(Math.floor(baseRotate * (Math.random() * 2 - 1)));
 
   let className =
-    'border p-4 px-4 shadow-sm rounded font-weight-bold d-flex align-items-center justify-content-between flex-column text-center';
+    'border p-4 px-4 shadow-sm rounded-lg font-weight-bold d-flex align-items-center justify-content-between flex-column text-center';
   if (!disabled) {
     className += ' bg-light text-muted font-weight-light';
   } else {
@@ -36,7 +66,13 @@ const Card = props => {
   }
 
   return (
-    <CardLi className={className} onClick={onClick} rotate={rotate}>
+    <CardLi
+      className={className}
+      onClick={onClick}
+      rotate={rotate}
+      face={face}
+      suit={suit}
+    >
       <SuitStringToComponent suit={suit} />
       <span>{face}</span>
     </CardLi>
