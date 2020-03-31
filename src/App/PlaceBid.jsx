b6fbdd3574c0bc536c3ea6a733bda144b3808cee
Mark Redeman <markredeman@gmail.com>
2020-03-31 13:06:44 +0200

    Improve place bid component

--- src/App/PlaceBid.jsx
+++ src/App/PlaceBid.jsx
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import { SuitStringToComponent } from 'Components/Suits';
 import { SUITES, SANS } from 'GameLogic/Card';
 import { canPlaceBid } from 'GameLogic/Phases/PlaceBids';
+import { Spades, Hearts, Diamonds, Clubs, Sans } from 'Components/SuitIcon';
+import _ from 'lodash';
+
 const { SPADES, HEARTS, CLUBS, DIAMONDS } = SUITES;
 
 export const CurrentBids = ({ bids, currentPlayer }) => {
@@ -25,19 +28,15 @@ export const CurrentBids = ({ bids, currentPlayer }) => {
 };
 
 export const Bid = ({ bid }) => {
-  if (bid === undefined) {
-    return <span>Waiting for bid...</span>;
-  }
-
-  if (bid === null) {
+  if (bid.bid === null) {
     return <span>Pass</span>;
   }
 
   return (
-    <div className="d-flex justify-content-center align-items-center mt-1">
+    <span className="">
       <strong className="mr-2">{bid.bid}</strong>
       <SuitStringToComponent suit={bid.suit} />
-    </div>
+    </span>
   );
 };
 
@@ -60,6 +59,9 @@ const PlaceBid = ({ placeBid, pass, currentBids, currentPlayer }) => {
     : [80, 90, 100, 110, 120, 130, 140, 150, 160, 'pit']
   ).filter(bid => canPlaceBid(currentBids, { bid, suit: trump }));
 
+  const bidCanBePlaced = ({ bid, trump }) =>
+    canPlaceBid(currentBids, { bid, suit: trump });
+
   if (!allowedBids.includes(bid)) {
     setBid(allowedBids[0]);
   }
@@ -74,41 +76,96 @@ const PlaceBid = ({ placeBid, pass, currentBids, currentPlayer }) => {
     setBid(parseInt(e.target.value, 10));
   };
 
+  const trumpOptionss = [
+    { trump: SPADES, component: <Spades /> },
+    { trump: HEARTS, component: <Hearts /> },
+    { trump: DIAMONDS, component: <Diamonds /> },
+    { trump: CLUBS, component: <Clubs /> },
+    { trump: SANS, component: <Sans /> }
+  ];
+
+  const bidsToShow = _.take([...currentBids].reverse(), 3);
+  console.log(bidsToShow);
   return (
-    <div className="d-flex flex-column w-100 text-left">
-      <h4>Place a bid</h4>
-      <div className="d-flex justify-content-between">
-        <div className="form-group">
-          <select
-            className="form-control form-control-sm"
-            id="trump"
-            onChange={changeTrump}
-            value={trump}
-          >
-            {trumpOptions.map(trump => (
-              <option key={trump} value={trump}>
-                {trumpLabel[trump]}
-              </option>
+    <div
+      className="d-flex flex-column w-50 text-left bg-white mx-auto shadow border my-3"
+      style={{ fontSize: '0.7rem', zIndex: '2' }}
+    >
+      <div className="p-3">
+        <h3 className="h5">Bidding round</h3>
+      </div>
+      {bidsToShow.length > 0 && (
+        <div className="p-3 bg-light border-top">
+          <h4 className="h6">Previous bids</h4>
+          <ul className="list-unstyled text-muted my-2">
+            {bidsToShow.map((bid, idx) => (
+              <li className="d-flex justify-content-between my-2" key={idx}>
+                <div>
+                  <strong>Player {bid.bidBy}</strong>:
+                </div>
+                <div>
+                  {bid.bidBy === currentPlayer ? (
+                    <span>Placing bid</span>
+                  ) : (
+                    <Bid bid={bid} />
+                  )}
+                </div>
+              </li>
             ))}
-          </select>
+          </ul>
         </div>
-        <div className="form-group">
-          <select
-            className="form-control form-control-sm"
-            id="bid"
-            onChange={changeBid}
-            value={bid}
-          >
-            {allowedBids.map(bid => (
-              <option key={bid} value={bid}>
-                {bid}
-              </option>
-            ))}
-          </select>
+      )}
+      <div className="bg-white border-top p-3">
+        <h4 className="h6">Place bid</h4>
+
+        <div className="d-flex  justify-content-between">
+          <div className="form-group flex-grow-1 mr-2">
+            <label for="">Trump</label>
+            <select
+              className="form-control form-control-sm"
+              id="trump"
+              onChange={changeTrump}
+              value={trump}
+            >
+              {trumpOptions.map(trump => (
+                <option key={trump} value={trump}>
+                  {trumpLabel[trump]}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="form-group flex-grow-1 ml-2">
+            <label for="">Bid</label>
+            <select
+              className="form-control form-control-sm"
+              id="bid"
+              onChange={changeBid}
+              value={bid}
+            >
+              {allowedBids.map(bid => (
+                <option key={bid} value={bid}>
+                  {bid}
+                </option>
+              ))}
+            </select>
+          </div>
         </div>
-        <div>
+      </div>
+      <div className="bg-white">
+        <div className="d-flex justify-content-between ">
           <button
-            className="btn btn-primary"
+            className="m-0 btn btn-sm btn-text text-muted btn-block bg-light p-3 px-3 "
+            type="submit"
+            onClick={() => {
+              const result = pass();
+              console.log({ result });
+              return;
+            }}
+          >
+            Pass
+          </button>
+          <button
+            className="m-0 btn btn-sm btn-text text-primary btn-block bg-light p-3 px-3"
             type="submit"
             onClick={() => {
               const result = placeBid({ suit: trump, bid });
@@ -119,22 +176,7 @@ const PlaceBid = ({ placeBid, pass, currentBids, currentPlayer }) => {
             Submit bid
           </button>
         </div>
-        <div>
-          <button
-            className="btn btn-primary"
-            type="submit"
-            onClick={() => {
-              const result = pass();
-              console.log({ result });
-              return;
-            }}
-          >
-            Pass
-          </button>
-        </div>
       </div>
-
-      <div className="d-flex justify-content-between"></div>
     </div>
   );
 };
