2436b8e5040f84604a00704aeb784936417c207b
Mark Redeman <markredeman@gmail.com>
2020-04-02 00:18:33 +0200

    Update score component

--- src/App/Score.jsx
+++ src/App/Score.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Bid } from 'App/PlaceBid';
 import styled from 'styled-components';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFileAlt } from '@fortawesome/free-solid-svg-icons';
 
 const ScoreContainer = styled.div`
   grid-area: s;
@@ -21,15 +19,19 @@ const Score = ({ game }) => {
         <table style={{ fontSize: '0.8rem' }} className="d-none">
           <thead>
             <tr>
-              <th colspan="2" className="text-right">
+              <th colSpan="2" className="text-right">
                 Wij
               </th>
-              <th colspan="2" className="text-left">
+              <th colSpan="2" className="text-left">
                 Zij
               </th>
             </tr>
           </thead>
           <tbody>
+            <tr>
+              <td className="text-right">{game.wij}</td>
+              <td className="text-left">{game.zij}</td>
+            </tr>
             <tr>
               <td>
                 (
@@ -39,8 +41,6 @@ const Score = ({ game }) => {
                   )}
                 )
               </td>
-              <td className="text-right">{game.wij}</td>
-              <td className="text-left">{game.zij}</td>
               <td>
                 (
                 {game.bid.trump !== undefined &&
@@ -67,7 +67,7 @@ const Score = ({ game }) => {
           <li className="text-muted p-1 d-flex flex-column text-right align-items-end">
             <strong>Wij</strong>
             <div className="d-flex justify-content-between">
-              <div className="mr-2 d-none">
+              <div className="mr-2">
                 {game.bid.trump !== undefined &&
                   [0, 2].includes(game.bid.highestBidBy) && (
                     <Bid bid={{ bid: game.bid.bid, suit: game.bid.trump }} />
@@ -81,7 +81,7 @@ const Score = ({ game }) => {
             <strong>Zij</strong>
             <div className="d-flex justify-content-between">
               <span>{game.zij}</span>
-              <div className="ml-2 d-none">
+              <div className="ml-2">
                 {game.bid.trump !== undefined &&
                   [1, 3].includes(game.bid.highestBidBy) && (
                     <Bid bid={{ bid: game.bid.bid, suit: game.bid.trump }} />
@@ -90,23 +90,7 @@ const Score = ({ game }) => {
             </div>
             <span>{game.zij}</span>
           </li>
-          <li>
-            {game.bid.trump !== undefined &&
-              [0, 2].includes(game.bid.highestBidBy) && (
-                <Bid bid={{ bid: game.bid.bid, suit: game.bid.trump }} />
-              )}
-          </li>
         </ul>
-        <div className="text-muted py-1 rounded d-none">
-          <button className="btn btn-text text-muted px-0">
-            <FontAwesomeIcon
-              icon={faFileAlt}
-              fixedWidth
-              className="text-muted"
-            />
-            view score
-          </button>
-        </div>
       </div>
     </ScoreContainer>
   );
