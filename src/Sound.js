b6922af7f8550566bcdea7d185e8d405dd1c0306
Mark Redeman <markredeman@gmail.com>
2020-04-03 09:36:12 +0200

    Play sound when placing a bid

--- src/Sound.js
+++ src/Sound.js
@@ -1,18 +1,28 @@
 import soundsMiddleware from 'redux-sounds';
 import playCardSfx from 'assets/sounds/card_play.mp3';
+import placeBidSfx from 'assets/sounds/place_bid.mp3';
 
 const soundsData = {
-  playCard: playCardSfx
+  playCard: playCardSfx,
+  placeBid: placeBidSfx
 };
 
 export const playSoundsMiddleware = store => next => action => {
   // NOTE: we only check if a PlayCard move is made, we don't yet check if it
   // is / was a valid move
-  if (action.type === 'MAKE_MOVE' && action.payload.type === 'PlayCard') {
-    return next({
-      ...action,
-      meta: { ...action.meta, sound: { play: 'playCard' } }
-    });
+  if (action.type === 'MAKE_MOVE') {
+    if (action.payload.type === 'PlayCard') {
+      return next({
+        ...action,
+        meta: { ...action.meta, sound: { play: 'playCard' } }
+      });
+    }
+    if (action.payload.type === 'PlaceBid' || action.payload.type === 'Pass') {
+      return next({
+        ...action,
+        meta: { ...action.meta, sound: { play: 'placeBid' } }
+      });
+    }
   }
 
   return next(action);
