5dd62413e43f17d926d244c6415f3a2da5614bcf
Mark Redeman <markredeman@gmail.com>
2020-04-04 13:11:45 +0200

    Use Modal in GameOver screen

--- src/App/GameOver.jsx
+++ src/App/GameOver.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Modal from 'Components/Modal';
 
 const GameOver = () => {
   const [showHoi, setShowHoi] = useState(false);
@@ -6,27 +7,25 @@ const GameOver = () => {
   console.log(showHoi);
 
   return (
-    <div className=" bg-white shadow rounded">
-      <div className="p-3">
-        <h3 className="h5 text-left">Game over</h3>
-
+    <Modal.Dialog>
+      <Modal.Header>
+        <Modal.Title>Game Over</Modal.Title>
         <p className="text-left text-muted mb-0">You have completed a tree.</p>
-      </div>
-      <div className="d-flex justify-content-between border-top">
+      </Modal.Header>
+      <Modal.Footer>
         {showHoi ? (
           <div className="btn btn-text text-muted bg-light btn-block m-0 p-3">
             Hoi
           </div>
         ) : (
-          <button
-            className="btn btn-text rounded-0 text-primary bg-light btn-block m-0 p-3"
-            onClick={() => setShowHoi(true)}
-          >
-            Please press this button
-          </button>
+          <Modal.Actions>
+            <Modal.Action onClick={() => setShowHoi(true)}>
+              Please press this button
+            </Modal.Action>
+          </Modal.Actions>
         )}
-      </div>
-    </div>
+      </Modal.Footer>
+    </Modal.Dialog>
   );
 };
 
