a8d500414ec12c07c8ea692927e5fd515df2cdaf
Mark Redeman <markredeman@gmail.com>
2020-04-04 13:08:18 +0200

    Use Modal in ShowResultOfHand

--- src/App/ShowResultOfHand.jsx
+++ src/App/ShowResultOfHand.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { SuitStringToComponent } from 'Components/Suits';
 import _ from 'lodash';
+import Modal from 'Components/Modal';
 
 const Prominent = ({ children }) => (
   <span>{children === 33 ? "'Vo" : children}</span>
@@ -18,6 +19,103 @@ export const Bid = ({ bid }) => {
     </span>
   );
 };
+
+const ResultTable = ({ round, bid }) => {
+  const [tricksByWij, tricksByZij] = _.partition(
+    round.playedTricks,
+    ({ winner }) => [0, 2].includes(winner)
+  );
+
+  const lastTrick = round.playedTricks[round.playedTricks.length - 1];
+
+  return (
+    <table className="table mb-0">
+      <thead>
+        <tr>
+          <th></th>
+          <th>Wij</th>
+          <th>Zij</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Points</td>
+          <td>
+            <Prominent>
+              {_(tricksByWij)
+                .map(({ points }) => points)
+                .sum()}
+            </Prominent>
+          </td>
+          <td>
+            <Prominent>
+              {_(tricksByZij)
+                .map(({ points }) => points)
+                .sum()}
+            </Prominent>
+          </td>
+        </tr>
+        <tr>
+          <td>Honor</td>
+          <td>
+            <Prominent>
+              {_(tricksByWij)
+                .map(({ honor }) => honor)
+                .sum()}
+            </Prominent>
+          </td>
+          <td>
+            <Prominent>
+              {_(tricksByZij)
+                .map(({ honor }) => honor)
+                .sum()}
+            </Prominent>
+          </td>
+        </tr>
+        <tr>
+          <td>Last trick</td>
+          <td>
+            <span>{[0, 2].includes(lastTrick.winner) ? '10' : '0'}</span>
+          </td>
+          <td>
+            <span>{[1, 3].includes(lastTrick.winner) ? '10' : '0'}</span>
+          </td>
+        </tr>
+        <tr>
+          <td>Bid</td>
+          <td>{[0, 2].includes(bid.highestBidBy) && <Bid bid={bid} />}</td>
+          <td>{[1, 3].includes(bid.highestBidBy) && <Bid bid={bid} />}</td>
+        </tr>
+      </tbody>
+      <tfoot>
+        <tr>
+          <th>Total</th>
+          <td>
+            <Prominent>{round.wij}</Prominent>
+          </td>
+          <td>
+            <Prominent>{round.zij}</Prominent>
+          </td>
+        </tr>
+      </tfoot>
+    </table>
+  );
+};
+
+const CheckContinueAutomatically = ({ checked, onChange }) => (
+  <div className="form-group form-check text-muted mb-0">
+    <input
+      type="checkbox"
+      className="form-check-input"
+      id="continueAutomatically"
+      checked={checked}
+      onChange={onChange}
+    />
+    <label className="form-check-label" htmlFor="continueAutomatically">
+      Continue next trick automatically
+    </label>
+  </div>
+);
 
 const ShowResultsOfHand = ({
   game,
@@ -42,127 +140,31 @@ const ShowResultsOfHand = ({
   }, [currentPlayer, continueTrickAutomatically, moves]);
 
   const round = game.rounds[game.rounds.length - 1];
-  const [tricksByWij, tricksByZij] = _.partition(
-    round.playedTricks,
-    ({ winner }) => [0, 2].includes(winner)
-  );
-  console.log({ tricksByWij, tricksByZij });
-
-  const lastTrick = round.playedTricks[round.playedTricks.length - 1];
 
   return (
-    <>
-      <div className="bg-white rounded shadow text-left" style={{ zIndex: 10 }}>
-        <div className="">
-          <div className="p-3">
-            <h3 className="h5">Finished hand</h3>
-          </div>
-          <table className="table mb-0">
-            <thead>
-              <tr>
-                <th></th>
-                <th>Wij</th>
-                <th>Zij</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>Points</td>
-                <td>
-                  <Prominent>
-                    {_(tricksByWij)
-                      .map(({ points }) => points)
-                      .sum()}
-                  </Prominent>
-                </td>
-                <td>
-                  <Prominent>
-                    {_(tricksByZij)
-                      .map(({ points }) => points)
-                      .sum()}
-                  </Prominent>
-                </td>
-              </tr>
-              <tr>
-                <td>Honor</td>
-                <td>
-                  <Prominent>
-                    {_(tricksByWij)
-                      .map(({ honor }) => honor)
-                      .sum()}
-                  </Prominent>
-                </td>
-                <td>
-                  <Prominent>
-                    {_(tricksByZij)
-                      .map(({ honor }) => honor)
-                      .sum()}
-                  </Prominent>
-                </td>
-              </tr>
-              <tr>
-                <td>Last trick</td>
-                <td>
-                  <span>{[0, 2].includes(lastTrick.winner) ? '10' : '0'}</span>
-                </td>
-                <td>
-                  <span>{[1, 3].includes(lastTrick.winner) ? '10' : '0'}</span>
-                </td>
-              </tr>
-              <tr>
-                <td>Bid</td>
-                <td>
-                  {[0, 2].includes(game.bid.highestBidBy) && (
-                    <Bid bid={game.bid} />
-                  )}
-                </td>
-                <td>
-                  {[1, 3].includes(game.bid.highestBidBy) && (
-                    <Bid bid={game.bid} />
-                  )}
-                </td>
-              </tr>
-            </tbody>
-            <tfoot>
-              <tr>
-                <th>Total</th>
-                <td>
-                  <Prominent>{round.wij}</Prominent>
-                </td>
-                <td>
-                  <Prominent>{round.zij}</Prominent>
-                </td>
-              </tr>
-            </tfoot>
-          </table>
-          <div className="p-3 border-top ">
-            <div className="form-group form-check text-muted mb-0">
-              <input
-                type="checkbox"
-                className="form-check-input"
-                id="continueAutomatically"
-                checked={continueAutomatically}
-                onChange={() =>
-                  setContinueAutomatically(!continueAutomatically)
-                }
-              />
-              <label
-                className="form-check-label"
-                htmlFor="continueAutomatically"
-              >
-                Continue next trick automatically
-              </label>
-            </div>
-          </div>
-        </div>
-        <button
-          className="m-0 btn btn-sm btn-text text-primary btn-block bg-light p-3 px-3"
-          onClick={() => moves.PlayNextHand(continueAutomatically)}
-        >
-          Play next hand
-        </button>
-      </div>
-    </>
+    <Modal.Dialog style={{ zIndex: 10 }}>
+      <Modal.Header>
+        <Modal.Title>Finished hand</Modal.Title>
+      </Modal.Header>
+      <Modal.Table>
+        <ResultTable round={round} bid={game.bid} />
+      </Modal.Table>
+      <Modal.Body className="border-top">
+        <CheckContinueAutomatically
+          checked={continueAutomatically}
+          onChange={() => setContinueAutomatically(!continueAutomatically)}
+        />
+      </Modal.Body>
+      <Modal.Footer>
+        <Modal.Actions>
+          <Modal.Action
+            onClick={() => moves.PlayNextHand(continueAutomatically)}
+          >
+            Play next hand
+          </Modal.Action>
+        </Modal.Actions>
+      </Modal.Footer>
+    </Modal.Dialog>
   );
 };
 
