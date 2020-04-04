6407438471acaec7eba2f825a15a526bfb4e9f8a
Mark Redeman <markredeman@gmail.com>
2020-04-04 12:00:46 +0200

    Add a Modal component

---
+++ src/Components/Modal.jsx
@@ -0,0 +1,115 @@
+import React, { useState } from 'react';
+import styled, { css } from 'styled-components';
+import classNames from 'classnames';
+
+const Modal = ({ className, ...props }) => {
+  return <div {...props} className={classNames(className)}></div>;
+};
+const Dialog = ({ children, className, ...props }) => {
+  const componentClasses = [
+    'd-flex',
+    'flex-column',
+    'w-50',
+    'text-left',
+    'bg-white',
+    'mx-auto',
+    'shadow',
+    'border my-3'
+  ];
+
+  return (
+    <div
+      {...props}
+      style={{ fontSize: '0.7rem', zIndex: '2' }}
+      className={classNames(componentClasses, className)}
+    >
+      {children}
+    </div>
+  );
+};
+const Header = ({ children, className, ...props }) => {
+  const componentClasses = ['p-3'];
+  return (
+    <div {...props} className={classNames(componentClasses, className)}>
+      {children}
+    </div>
+  );
+};
+const Title = ({ children, className, ...props }) => {
+  const componentClasses = ['h5'];
+  return (
+    <h3 {...props} className={classNames(componentClasses, className)}>
+      {children}
+    </h3>
+  );
+};
+const Body = ({ children, className, ...props }) => {
+  const componentClasses = ['p-3'];
+  return (
+    <div {...props} className={classNames(componentClasses, className)}>
+      {children}
+    </div>
+  );
+};
+const Table = ({ className, ...props }) => {
+  return <div {...props} className={classNames(className)}></div>;
+};
+
+const Footer = ({ children, className, ...props }) => {
+  const componentClasses = ['bg-light', 'text-muted'];
+  return (
+    <div {...props} className={classNames(componentClasses, className)}>
+      {children}
+    </div>
+  );
+};
+const Actions = ({ children, className, ...props }) => {
+  const componentClasses = ['d-flex', 'justify-content-between'];
+  return (
+    <div className="bg-white">
+      <div {...props} className={classNames(componentClasses, className)}>
+        {children}
+      </div>
+    </div>
+  );
+};
+const Action = ({
+  children,
+  onClick,
+  type = 'submit',
+  className,
+  ...props
+}) => {
+  const componentClasses = [
+    'm-0',
+    'btn',
+    'btn-sm',
+    'btn-text',
+    'text-primary',
+    'btn-block',
+    'bg-light',
+    'p-3',
+    'px-3'
+  ];
+  return (
+    <button
+      onClick={onClick}
+      type={type}
+      {...props}
+      className={classNames(componentClasses, className)}
+    >
+      {children}
+    </button>
+  );
+};
+
+Modal.Dialog = Dialog;
+Modal.Header = Header;
+Modal.Title = Title;
+Modal.Body = Body;
+Modal.Table = Table;
+Modal.Footer = Footer;
+Modal.Actions = Actions;
+Modal.Action = Action;
+
+export default Modal;
