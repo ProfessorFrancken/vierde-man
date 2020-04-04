e42c258d7ee66a7fb7325b77e5ab7ef19a0d2657
Mark Redeman <markredeman@gmail.com>
2020-04-04 11:06:05 +0200

    Remove SuitIcon.jsx

--- src/Components/Suits.jsx
+++ src/Components/Suits.jsx
@@ -1,5 +1,30 @@
 import React from 'react';
-import { Spades, Hearts, Diamonds, Clubs, Sans } from 'Components/SuitIcon';
+import sans from 'assets/Sans2.png';
+import styled from 'styled-components';
+
+const SuitIcon = props => {
+  const imgStyle = {
+    width: props.width ? props.width : '20px'
+  };
+
+  return <img src={props.src} style={imgStyle} alt={props.alt} />;
+};
+
+const red = '#e44145';
+const black = '#252525';
+
+const RedSuit = styled.span`
+  color: ${red};
+`;
+const BlackSuit = styled.span`
+  color: ${black};
+`;
+
+export const Spades = () => <BlackSuit>♠</BlackSuit>;
+export const Hearts = () => <RedSuit>♥</RedSuit>;
+export const Diamonds = () => <RedSuit>♦</RedSuit>;
+export const Clubs = () => <BlackSuit>♣</BlackSuit>;
+export const Sans = () => <SuitIcon src={sans} width="60px" alt="Sans" />;
 
 export const SuitStringToComponent = ({ suit }) => {
   switch (suit) {
@@ -37,5 +62,3 @@ export const Bid = props => (
     <SuitStringToComponent suit={props.suit} />
   </span>
 );
-
-export { Spades, Hearts, Diamonds, Clubs, Sans };
