510134f34ae6d6f3fc703000020f47664fee06eb
Mark Redeman <markredeman@gmail.com>
2020-04-07 12:44:31 +0200

    Add public environment variable

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 const config = {
   gameServer: process.env.REACT_APP_GAME_SERVER,
-  lobbyServer: process.env.REACT_APP_LOBBY_SERVER
+  lobbyServer: process.env.REACT_APP_LOBBY_SERVER,
+  public: process.env.REACT_APP_PUBLIC || false
 };
 
 export default config;
