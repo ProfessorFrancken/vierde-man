713995f8cde2f2abf50e6aced596a32d50df91ca
Mark Redeman <markredeman@gmail.com>
2020-04-07 20:38:14 +0200

    Correctly convert public config

--- src/App.js
+++ src/App.js
@@ -127,7 +127,7 @@ const App = () => (
             </AppContainer>
           </Route>
           <Route>
-            {config.public ? (
+            {config.public === true ? (
               <Redirect to="/lobby" />
             ) : (
               <AppContainer>
--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const config = {
   gameServer: process.env.REACT_APP_GAME_SERVER,
   lobbyServer: process.env.REACT_APP_LOBBY_SERVER,
-  public: process.env.REACT_APP_PUBLIC || false
+  public: process.env.REACT_APP_PUBLIC === 'true' || false
 };
 
 export default config;
