e2bce969f9f7518a042e005be077e67176747cc0
Mark Redeman <markredeman@gmail.com>
2020-04-11 10:15:15 +0200

    Add useInterval hook
    See https://overreacted.io/making-setinterval-declarative-with-react-hooks/

--- src/hooks.js
+++ src/hooks.js
@@ -1,8 +1,8 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
 export const useOnClickOutside = (ref, handler) => {
   useEffect(() => {
-    const listener = event => {
+    const listener = (event) => {
       if (!ref.current || ref.current.contains(event.target)) {
         return;
       }
@@ -14,4 +14,24 @@ export const useOnClickOutside = (ref, handler) => {
       document.removeEventListener('mousedown', listener);
     };
   }, [ref, handler]);
+};
+
+export const useInterval = (callback, delay) => {
+  const savedCallback = useRef();
+
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
 };
