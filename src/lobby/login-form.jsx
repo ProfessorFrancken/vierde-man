2b1cd13cdfb943046c654a0955a7925125415eeb
Mark Redeman <markredeman@gmail.com>
2020-04-11 16:19:55 +0200

    Make login form a functional component

--- src/lobby/login-form.jsx
+++ src/lobby/login-form.jsx
@@ -1,81 +1,65 @@
-/*
- * Copyright 2018 The boardgame.io Authors.
- *
- * Use of this source code is governed by a MIT-style
- * license that can be found in the LICENSE file or at
- * https://opensource.org/licenses/MIT.
- */
-
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Modal from 'Components/Modal';
 
-class LobbyLoginForm extends React.Component {
-  static propTypes = {
-    playerName: PropTypes.string,
-    onEnter: PropTypes.func.isRequired
-  };
-  static defaultProps = {
-    playerName: ''
-  };
+const Login = (props) => {
+  const onEnter = props.onEnter;
+  const [playerName, setPlayerName] = useState(props.playerName || '');
+  const [nameErrorMsg, setNameErrorMsg] = useState();
 
-  state = {
-    playerName: this.props.playerName,
-    nameErrorMsg: ''
+  const onClickEnter = () => {
+    if (playerName === '') {
+      return;
+    }
+    onEnter(playerName.trim());
   };
 
-  render() {
-    return (
-      <div className="container my-5">
-        <Modal.Dialog style={{}}>
-          <Modal.Header>
-            <Modal.Title>Choose a player name: </Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <div className="form-group">
-              <input
-                type="text"
-                value={this.state.playerName}
-                onChange={this.onChangePlayerName}
-                onKeyPress={this.onKeyPress}
-                className="form-control"
-              />
-
-              {this.state.nameErrorMsg && (
-                <div className="invalid-feedback">
-                  {this.state.nameErrorMsg}
-                </div>
-              )}
-            </div>
-          </Modal.Body>
-          <Modal.Footer>
-            <Modal.Actions>
-              <Modal.Action onClick={this.onClickEnter}>Enter</Modal.Action>
-            </Modal.Actions>
-          </Modal.Footer>
-        </Modal.Dialog>
-      </div>
-    );
-  }
-
-  onClickEnter = () => {
-    if (this.state.playerName === '') return;
-    this.props.onEnter(this.state.playerName);
-  };
-
-  onKeyPress = event => {
+  const onKeyPress = (event) => {
     if (event.key === 'Enter') {
-      this.onClickEnter();
+      onClickEnter();
     }
   };
 
-  onChangePlayerName = event => {
-    const name = event.target.value.trim();
-    this.setState({
-      playerName: name,
-      nameErrorMsg: name.length > 0 ? '' : 'empty player name'
-    });
+  const onChangePlayerName = (event) => {
+    const name = event.target.value;
+    setPlayerName(name);
+    setNameErrorMsg(name.length > 0 ? '' : 'empty player name');
   };
-}
 
-export default LobbyLoginForm;
+  return (
+    <div className="container my-5">
+      <Modal.Dialog style={{}}>
+        <Modal.Header>
+          <Modal.Title>Choose a player name: </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className="form-group">
+            <input
+              type="text"
+              value={playerName}
+              onChange={onChangePlayerName}
+              onKeyPress={onKeyPress}
+              className="form-control"
+            />
+
+            {nameErrorMsg && (
+              <div className="invalid-feedback">{nameErrorMsg}</div>
+            )}
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Modal.Actions>
+            <Modal.Action onClick={onClickEnter}>Enter</Modal.Action>
+          </Modal.Actions>
+        </Modal.Footer>
+      </Modal.Dialog>
+    </div>
+  );
+};
+
+Login.propTypes = {
+  playerName: PropTypes.string,
+  onEnter: PropTypes.func.isRequired,
+};
+
+export default Login;
