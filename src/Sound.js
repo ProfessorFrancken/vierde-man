62efb96a56bc2f56a6d2feea018fb03568f372f8
Mark Redeman <markredeman@gmail.com>
2020-04-14 17:53:38 +0200

    Play sounds after any play card or place bid move

--- src/Sound.js
+++ src/Sound.js
@@ -4,28 +4,31 @@ import placeBidSfx from 'assets/sounds/place_bid.mp3';
 
 const soundsData = {
   playCard: playCardSfx,
-  placeBid: placeBidSfx
+  placeBid: placeBidSfx,
 };
 
-export const playSoundsMiddleware = store => next => action => {
-  // NOTE: we only check if a PlayCard move is made, we don't yet check if it
-  // is / was a valid move
-  if (action.type === 'MAKE_MOVE') {
-    if (action.payload.type === 'PlayCard') {
-      return next({
-        ...action,
-        meta: { ...action.meta, sound: { play: 'playCard' } }
-      });
-    }
-    if (action.payload.type === 'PlaceBid' || action.payload.type === 'Pass') {
-      return next({
-        ...action,
-        meta: { ...action.meta, sound: { play: 'placeBid' } }
-      });
-    }
-  }
+export const playSoundsMiddleware = (store) => (next) => (action) => {
+  // For each move we define a sound that should be played after this move
+  const payloadTypeToSound = {
+    PlayCard: { play: 'playCard' },
+    PlaceBid: { play: 'placeBid' },
+    Pass: { play: 'placeBid' },
+  };
 
-  return next(action);
+  // We want to (possibly) play a sound after every game upate, where the
+  // deltalog contains the moves made in a game update
+  const sounds =
+    action.type !== 'UPDATE'
+      ? []
+      : action.deltalog
+          .filter(({ action }) => action.payload.type in payloadTypeToSound)
+          .map(({ action }) => payloadTypeToSound[action.payload.type]);
+
+  // I haven't yet figured out how to play more than 1 sound at a time using,
+  // the meta keys, so we will pick the first sound that should be played
+  const sound = sounds.pop();
+
+  return next({ ...action, meta: { ...action.meta, sound } });
 };
 
 // Pre-load our middleware with our sounds data.
