7c64f25219b02393dfcb73e4d91cf52e6a3b8482
Mark Redeman <markredeman@gmail.com>
2020-04-17 21:51:06 +0200

    Overwrite /games/:name route
    We now also return part of the state of a game so that we can filter out
    finished and ongoing games.

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,8 @@
 import { FlatFile, Server } from 'boardgame.io/dist/server';
 import { KlaverJassen } from './../GameLogic/Game';
 import { StorageCache } from 'bgio-storage-cache';
+import Router from 'koa-router';
+import cors from '@koa/cors';
 
 const db = new FlatFile({
   dir: './storage/klaverjas',
@@ -12,6 +14,43 @@ const server = Server({
   games: [KlaverJassen],
   db: dbWithCaching,
 });
+
+const router = new Router();
+router.get('/games/:name', async (ctx) => {
+  const gameName = ctx.params.name;
+  const gameList = await dbWithCaching.listGames(gameName);
+  let rooms = [];
+  for (let gameID of gameList) {
+    const { metadata, state } = await dbWithCaching.fetch(gameID, {
+      state: true,
+      metadata: true,
+    });
+    const G = state.G;
+
+    rooms.push({
+      gameID,
+      phase: state.ctx.phase,
+      startedAt: G.startedAt,
+      roundsPlayed: G.rounds.length,
+      wij: G.wij,
+      zij: G.zij,
+      currentPlayer: state.ctx.currentPlayer,
+
+      // strip away credentials
+      players: Object.values(metadata.players).map((player) => ({
+        id: player.id,
+        name: player.name,
+      })),
+      setupData: metadata.setupData,
+    });
+  }
+  ctx.body = {
+    rooms: rooms,
+  };
+});
+
+// TODO: verify if this is needed for the koa app
+server.app.use(cors()).use(router.routes()).use(router.allowedMethods());
 
 const PORT = process.env.PORT || 8000;
 server.run(PORT, () => {
